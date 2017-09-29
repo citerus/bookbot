d1b23644800ada3114c94bf1a3c21a2a237231d3
Erika Ornstein <keiwra@gmail.com>
2017-09-29 16:01:36 +0200

    App can now has a servlet after following https://guides.gradle.org/building-java-web-applications/.

--- src/main/java/hello/HelloJohan.java
+++ src/main/java/hello/HelloJohan.java
@@ -3,6 +3,6 @@ package hello;
 public class HelloJohan {
     public static void main(String[] args) {
         Greeter greeter = new Greeter();
-        System.out.println(greeter.sayHello());
+        greeter.sayHello();
     }
 }
