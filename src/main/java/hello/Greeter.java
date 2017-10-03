9cf6b910d102a7cc68280004b02a9b19cb897b72
Erika Ornstein <keiwra@gmail.com>
2017-10-03 21:41:53 +0200

    Greeter now returns Hello, World when name is empty.

--- src/main/java/hello/Greeter.java
+++ src/main/java/hello/Greeter.java
@@ -5,7 +5,9 @@ public class Greeter {
         return "Hello, Johan";
     }
     public String sayHello(String name) {
-        if (name == null) name = "World";
+        if (name.isEmpty()) {
+            name = "World";
+        }
         return "Hello, " + name;
     }
 
